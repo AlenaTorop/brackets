66f2c61f4f65183784399c6549738425323951c9
Alena Torop <alena.kudinova.1003@yandex.ru>
2022-08-02 17:48:47 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let closeBrackets = bracketsConfig[j][1];
+      let openBrackets = bracketsConfig[j][0];
+
+    if (stack.length === 0) {
+        if (str[i] === openBrackets) {
+            stack.push(str[i]);
+        } else {
+            if (str[i] === closeBrackets) {
+                return false;
+            }
+        }
+    } else {
+        if (str[i] === closeBrackets) {
+          let topElement = stack[stack.length - 1];
+            if (topElement === openBrackets) {
+                stack.pop();
+            } else {
+                if (openBrackets === closeBrackets) {
+                    stack.push(str[i]);
+                }
+                else { 
+                  return false; 
+                }
+            }
+        }
+        else {
+            if (str[i] === openBrackets) {
+                stack.push(str[i]);
+            }
+        }
+    }
+
+    }
+    }
+    return stack.length === 0;
+    }
+    
+    
+    
